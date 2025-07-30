709a57af7db39cabf0cc68ae61d930153c98c138
SeBoe3000 <selina.boettger@gmx.de>
2025-07-30 17:22:40 +0200

    Anpassung Formular styletechnisch.

--- ihk-hillmer-kuchen/php/kontakt.php
+++ ihk-hillmer-kuchen/php/kontakt.php
@@ -10,54 +10,64 @@ $formularAbgeschickt = ($_SERVER["REQUEST_METHOD"] === "POST");
 
 <?php if (!$formularAbgeschickt): ?>
     <p>Zur Kontaktaufnahme, füllen Sie bitte folgendes Formular aus:</p>
-    <form method="post">
-        <label for="anrede">Anrede</label>
-        <select name="ar" required id="anrede">
-            <option value="" disabled selected>Bitte auswählen</option>
-            <option value="Frau">Frau</option>
-            <option value="Herr">Herr</option>
-        </select><br><br>
-
-        <label for="vorname">Vorname</label>
-        <input type="text" placeholder="Vorname angeben" name="vn" required id="vorname"><br><br>
-
-        <label for="nachname">Nachname</label>
-        <input type="text" placeholder="Nachname angeben" name="nn" required id="nachname"><br><br>
-
-        <label for="mail">Mailadresse</label>
-        <input type="email" placeholder="E-Mail-Adresse angeben" name="em" required id="mail"><br><br>
-
-        <label for="betreff">Betreff</label>
-        <input type="text" placeholder="Betreff angeben" name="bet" required id="betreff"><br><br>
-
-        <label for="beschreibung">Beschreibung</label>
-        <textarea placeholder="Beschreibung angeben" name="bes" required id="beschreibung"></textarea><br><br>
-
-        <label for="nummer">Telefonnummer</label>
-        <input type="text" placeholder="Telefonnummer angeben" name="tel" required id="nummer"><br><br>
-
-        <input type="checkbox" name="dsgvo" required id="datenschutz">
-        <label for="datenschutz">Zustimmung zur Verarbeitung der Daten (<a href="datenschutz.php">DSGVO</a>)</label><br><br>
-
-        <button>Absenden</button>
-        <button type="reset">Reset</button><br><br>
+    <form class="form-container" method="post">
+        <div class="form-group">
+            <label for="anrede">Anrede</label>
+            <select name="ar" required id="anrede">
+                <option value="" disabled selected>Bitte auswählen</option>
+                <option value="Frau">Frau</option>
+                <option value="Herr">Herr</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="vorname">Vorname</label>
+            <input type="text" placeholder="Vorname angeben" name="vn" required id="vorname">
+        </div>
+        <div class="form-group">
+            <label for="nachname">Nachname</label>
+            <input type="text" placeholder="Nachname angeben" name="nn" required id="nachname">
+        </div>
+        <div class="form-group">
+            <label for="mail">Mailadresse</label>
+            <input type="email" placeholder="E-Mail-Adresse angeben" name="em" required id="mail">
+        </div>
+        <div class="form-group">
+            <label for="betreff">Betreff</label>
+            <input type="text" placeholder="Betreff angeben" name="bet" required id="betreff">
+        </div>
+        <div class="form-group">
+            <label for="beschreibung">Beschreibung</label>
+            <textarea placeholder="Beschreibung angeben" name="bes" required id="beschreibung"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="nummer">Telefonnummer</label>
+            <input type="text" placeholder="Telefonnummer angeben" name="tel" required id="nummer">
+        </div>
+        <div class="form-checkbox">
+            <input type="checkbox" name="dsgvo" required id="datenschutz">
+            <label for="datenschutz">Zustimmung zur Verarbeitung der Daten (<a href="datenschutz.php">DSGVO</a>)</label>
+        </div>
+        <div class="form-button">
+            <button>Absenden</button>
+            <button type="reset">Reset</button>
+        </div>
     </form>
 
 <?php else: ?>
     <?php
-        $anrede = $_POST["ar"] ?? "";
-        $vorname = $_POST["vn"] ?? "";
-        $nachname = $_POST["nn"] ?? "";
-        $mail = $_POST["em"] ?? "";
-        $betreff = $_POST["bet"] ?? "";
-        $beschreibung = $_POST["bes"] ?? "";
-        $telefonnummer = $_POST["tel"] ?? "";
-        $Datenschutz  = $_POST["dsgvo"] ?? "";
+    $anrede = $_POST["ar"] ?? "";
+    $vorname = $_POST["vn"] ?? "";
+    $nachname = $_POST["nn"] ?? "";
+    $mail = $_POST["em"] ?? "";
+    $betreff = $_POST["bet"] ?? "";
+    $beschreibung = $_POST["bes"] ?? "";
+    $telefonnummer = $_POST["tel"] ?? "";
+    $Datenschutz  = $_POST["dsgvo"] ?? "";
 
-        echo "Hallo $anrede $vorname $nachname wir haben ihre Nachricht mit dem Betreff \"$betreff\" und folgender Beschreibung erhalten: <br>";
-        echo "\"$beschreibung\"<br>";
-        echo "Wir werden uns schnellstmöglichst bei Ihnen melden, unter der Telefonnummer $telefonnummer bzw. E-Mail-Adresse $mail.";
-     ?>
+    echo "Hallo $anrede $vorname $nachname wir haben ihre Nachricht mit dem Betreff \"$betreff\" und folgender Beschreibung erhalten: <br>";
+    echo "\"$beschreibung\"<br>";
+    echo "Wir werden uns schnellstmöglichst bei Ihnen melden, unter der Telefonnummer $telefonnummer bzw. E-Mail-Adresse $mail.";
+    ?>
 <?php endif; ?>
 
 <?php include("includes/grund-ende.inc.php"); ?>
--- ihk-hillmer-kuchen/php/templates/header.php
+++ ihk-hillmer-kuchen/php/templates/header.php
@@ -5,7 +5,7 @@ include_once __DIR__ . '/../includes/funktionen.php';
 <header>
     <nav>
         <ul>
-            <li><a href="<?= $relPath ?>php/datenschutz.php">Header 1</a></li>
+            <li><a href="<?= $relPath ?>index.php">Start</a></li>
             <li><a href="<?= $relPath ?>php/impressum.php">Header 2</a></li>
             <li><a href="<?= $relPath ?>php/kontakt.php">Header 3</a></li>
             <li><?php echo getMode($mode)?></li>
